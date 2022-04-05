bfef35fa0db6fbc8074471377fdd002b33248380
Alex Pereira <alex.pereira.6464@gmail.com>
2022-04-05 11:20:08 -0400

    Updated to Master Copy
    Added in the drive functions, object declarations, and imports.

--- Catapult/src/main/java/org/firstinspires/ftc/teamcode/Controls.java
+++ Catapult/src/main/java/org/firstinspires/ftc/teamcode/Controls.java
@@ -1,33 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-//import the OpMode library
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class Controls {
     /* Class Variables */
     double driveSpeedMultiplier  = 1.00;
     
     //Object Creation
-    //Create an OpMode object here
+    private OpMode logitech;
 
     /**
      * Constructor
      */
     //Incomplete. Add the OpMode parameter
-    public Controls() {
+    public Controls(OpMode opMode) {
         //Passes an OpMode into the class (allows for the use of controllers)
-        //pass OpMode to the object here
+        logitech = opMode;
     }
 
     /**
      * Gamepad 1
      */
-    //Determine how you want to drive the robot and assign the controls to gamepad 1
+    public double leftDrivePower() {
+        double power = logitech.gamepad1.left_stick_y;
+        double drivePower = power * driveSpeedMultiplier;
+
+        return drivePower;
+    }
+
+    public double rightDrivePower() {
+        double power = logitech.gamepad1.right_stick_y;
+        double turnPower = power * driveSpeedMultiplier;
+
+        return turnPower;
+    }
 
     /**
      * Gamepad 2
      */
-    //Determine the button you want to use for shooting on gamepad 2
-
+    public boolean launchBall() {
+        return logitech.gamepad1.a;
+    }
+    
 }
 
 //End of the Controls class
