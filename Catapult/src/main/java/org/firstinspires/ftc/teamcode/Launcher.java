6053681dfc271dc5965446c788f186309f23f0dc
Alex Pereira <alex.pereira.6464@gmail.com>
2022-04-05 18:11:21 -0400

    Updated to Master Copy
    Added the logic for the launcher control.

--- Catapult/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
+++ Catapult/src/main/java/org/firstinspires/ftc/teamcode/Launcher.java
@@ -2,7 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 public class Grabber {
     /* Class Variables */
-    //Nothing yet...
+    private double prevEncoder = 0;
+    private double targetEncoder = 0;
+    
+    /* Constants */
+    private final double TOLERANCE = 15;
+    private final double ticksPerRot = 0; // This value is currently unknown
+    private final double ticksPerQuarterRot = ticksPerRot / 4;
 
     //Object Creation
     HardwareRobot robot;
@@ -11,21 +17,31 @@ public class Grabber {
      * Constructor
      */
     public Grabber() {
-        //Instance Creation
+        // Instance Creation
         robot = HardwareRobot.getInstance();
     }
     
     /**
-     * 
+     * The main method to shoot
      */
     public void shoot(boolean fire) {
-        if () {
-            //
+        double currEncoder = robot.launcherPosition();
+        
+        if ((fire == true) && ((currEncoder <= targetEncoder + TOLERANCE) && (currEncoder >= targetEncoder - TOLERANCE))) { // The signs are still unknown
+            prevEncoder = robot.launcherPosition();
         }
-        if (fire == true) {
-            robot.launcher.setEncoder();
+        
+        targetEncoder = prevEncoder + ticksPerQuarterRot; // The sign before ticksPerQuarterRot is still unknown
+        
+        if (currEncoder < targetEncoder) { // The sign is unknown
+            // The motor has not passed the target encoder
+            robot.launcher.set(0.15);
+        }
+        else {
+            // The motor has passed the target encoder
+            robot.launcher.set(0.0);
         }
     }
 }
 
-//End of the Manipulator class
+//End of the Launcher class
